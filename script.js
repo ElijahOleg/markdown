d298e9398e7b32a9d80e1762eb51539ca3c73d84
Elijah <ejoleg@gmail.com>
2015-06-25 09:21:11 -0700

    still have a bug in the communication between node and angular

---
+++ script.js
@@ -0,0 +1,25 @@
+var fs = require("fs"),
+    http = require("http"),
+    url = require('url'),
+    exec = require("child_process").exec,
+    marked = require('marked');
+
+
+http.createServer(responseHandler).listen(8888);
+
+
+function responseHandler(req, res) {
+  if (req.url.match("/")) {
+    res.writeHead(200, {"Content-Type": "text/html"});
+    fs.readFile('index.html', 'utf8', function (err,data) {
+      res.write(data);
+      res.end();
+    });
+  }else if(req.url.match(/\/markdown\//)){
+    var content = req.url.match(/\/markdown\/(.*)/)[1];
+    console.log(content);
+    var markedup = marked(decodeURIComponent(content));
+    res.write(markedup);
+    res.end();
+  }
+}
